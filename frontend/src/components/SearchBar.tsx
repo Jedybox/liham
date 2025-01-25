491274f549b0be4141db8fc1003ea52b5d13b0b4
Jedycode <jhonericssonytac365@gmail.com>
2025-01-25 13:59:10 +0800

    Add SearchBar component to Sidebar; implement search functionality and enhance styling

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,39 @@
+import { SearcIcon } from "./SVGIcons";
+import { useState } from "react";
+
+interface SearchBarProps {
+    subject: string;
+    onChangeSubject: (e: string) => void;
+    search: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+function SearchBar({ subject, onChangeSubject, search } : SearchBarProps): JSX.Element {
+  
+  const [focused, setFocused] = useState<boolean>(false);
+
+    return (
+    <form 
+        className="flex flex-row items-center justify-center w-full h-fit py-2 bg-primary rounded-full px-3 gap-1 overflow-hidden border-black border-[1px]"
+        onSubmit={search}
+    >
+      <SearcIcon focused={focused}/>
+      <input
+        onFocus={(e) => {
+            setFocused(true);
+            e.target.placeholder = "";
+        }}
+        onBlur={(e) => {
+            setFocused(false);
+            e.target.placeholder = "Search";
+        }}
+        className="w-full h-ful bg-transparent focus:outline-none focus:border-none"
+        type="text"
+        placeholder="Search"
+        value={subject}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChangeSubject(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchBar;
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,15 +1,33 @@
 // import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import Nav from "./Nav";
+import SearchBar from "./SearchBar";
 
 function SideBar(): JSX.Element {
 
   // const navigate = useNavigate();
 
+  const [subject, setSubject] = useState<string>("");
+
+  const search = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // navigate(`/search/${subject}`);
+
+    alert(`Searching for ${subject}`);
+  }
+
   return (
-    <div className="relative flex flex-col h-full w-1/4 p-5">
-      <div className="w-full h-full"></div>
+    <aside className="relative flex flex-col h-full w-1/4 p-5">
+      <SearchBar 
+        subject={subject} 
+        onChangeSubject={setSubject}
+        search={search}
+      />
+      <div className="w-full h-full">
+        
+      </div>
       <Nav />
-    </div>
+    </aside>
   );
 }
 
